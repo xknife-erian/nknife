1776e01878aeaada938131c5e4aaaa24331698dc
lukan <lukan@xknife.net>
2013-12-27 00:50:06 +0800

    加入NetUtil，加入一个简单判断本机IP地址的该法

---
+++ jknife-lang/src/main/java/net/xknife/lang/utilities/NetUtil.java
@@ -0,0 +1,38 @@
+package net.xknife.lang.utilities;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * Created by lukan@xknife.net on 13-12-27.
+ */
+public class NetUtil
+{
+
+    /**
+     * 获取本机的IP地址
+     *
+     * @return
+     * @throws SocketException
+     */
+    public static String getLocalIpAddress() throws SocketException
+    {
+        String ipAddress = "";
+
+        for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();)
+        {
+            NetworkInterface net = en.nextElement();
+            for (Enumeration<InetAddress> ip = net.getInetAddresses(); ip.hasMoreElements();)
+            {
+                InetAddress inetAddress = ip.nextElement();
+                if (!inetAddress.isLoopbackAddress())
+                {
+                    ipAddress = inetAddress.getHostAddress();
+                }
+            }
+        }
+        return ipAddress;
+    }
+}
