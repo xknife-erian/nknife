345e73d5172c0533400c36701a72fde147c16185
huangyang@xknife.net <huangyang@xknife.net>
2014-01-02 11:08:39 +0800

    again test hy 1

--- jknife-inject/src/main/java/gittest/aaa1.java
+++ jknife-inject/src/main/java/gittest/aaa1.java
@@ -8,11 +8,18 @@ public class aaa1
     public void 芦侃增加的函数1()
     {
         //这是我增加的函数。（lukan）
+        //sdfsdfsdf
+
+        System.out.println("hy do it");
     }
 
     public void 芦侃增加的函数2()
     {
         //这是我增加的函数。（lukan）
+    }
+
+    public void testset(){
+
     }
 
 }
