3ed30b8fa77c281855bb4bb94afa8c81682d7b05
huangyang-home <huangyang@jeelu.com>
2014-01-12 22:56:54 +0800

    增加对ICSharpCode库的引用

---
+++ NKnife/Platform/UtilityReflection.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Reflection;
+
+namespace NKnife.Platform
+{
+    public class UtilityReflection
+    {
+        /// <summary>
+        /// 通过反射执行指定对象的指定方法
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="methodName"></param>
+        /// <param name="paramList"></param>
+        public static void InvokeMethod(object target,string methodName,object[] paramList,BindingFlags bindingFlags)
+        {
+            MethodInfo methodInfo = target.GetType().GetMethod(methodName, bindingFlags);
+            if (methodInfo == null) throw new Exception(string.Format("找不到方法 '{0}'.", methodName)); ;
+            methodInfo.Invoke(target, paramList);
+        }
+
+        public static void InvokePrivateMethod(object target, string methodName, object[] paramList)
+        {
+            InvokeMethod(target, methodName, paramList, BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        /// <summary>
+        /// 通过反射找到指定对象的相关事件，并处理
+        /// </summary>
+        public static void AttatchAllEvent(object target, object handlerHost, string handlerMethodName)
+        {
+            foreach (EventInfo eventInfo in target.GetType().GetEvents())
+            {
+                try
+                {
+                    Delegate d = GetEventHandler(eventInfo,handlerHost,handlerMethodName);
+                    eventInfo.AddEventHandler(target, d);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private static Delegate GetEventHandler(EventInfo eventInfo,object handlerHost,string handlerMethodName)
+        {
+            try
+            {
+                if (eventInfo == null)
+                    throw new Exception(string.Format("Unable to find an event named '{0}'.", eventInfo.Name));
+                Type[] delegateParameters = GetDelegateParameterTypes(eventInfo.EventHandlerType);
+                if (delegateParameters == null || delegateParameters.Length != 2)
+                    throw new InvalidOperationException(string.Format("Event '{0}' is not valid.", eventInfo.Name));
+                Type eventArgsType = delegateParameters[1];
+                MethodInfo doEventMethod = handlerHost.GetType().GetMethod(handlerMethodName, BindingFlags.NonPublic | BindingFlags.Instance);
+                if (doEventMethod == null)
+                    throw new Exception(string.Format("{0} method doesn't exist.",handlerMethodName));
+                if (!doEventMethod.IsGenericMethod)
+                    throw new Exception(string.Format("{0} method is not a generic method.",handlerMethodName));
+                MethodInfo concreteDoEventMethod = doEventMethod.MakeGenericMethod(eventArgsType);
+                Delegate d = Delegate.CreateDelegate(eventInfo.EventHandlerType, handlerHost, concreteDoEventMethod);
+                return d;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 获得委托的参数类型
+        /// </summary>
+        private static Type[] GetDelegateParameterTypes(Type d)
+        {
+            if (d.BaseType != typeof(MulticastDelegate))
+            {
+                throw new InvalidOperationException("Not a delegate.");
+            }
+            MethodInfo invoke = d.GetMethod("Invoke");
+            if (invoke == null)
+            {
+                throw new InvalidOperationException("Not a delegate.");
+            }
+            ParameterInfo[] parameters = invoke.GetParameters();
+            Type[] typeParameters = new Type[parameters.Length];
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                typeParameters[i] = parameters[i].ParameterType;
+            }
+            return typeParameters;
+        }
+    }
+}
