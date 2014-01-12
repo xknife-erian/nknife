a08e42de7f248cf52e0fdb84b1caa5e4f8580751
huangyang-home <huangyang@jeelu.com>
2014-01-12 21:17:50 +0800

    增加了GUI内容

---
+++ NKnife/GUI/Controls/ISplashForm.cs
@@ -0,0 +1,10 @@
+namespace NKnife.GUI.Controls
+{
+    /// <summary>
+    /// interface for Splash Screen
+    /// </summary>
+    public interface ISplashForm
+    {
+        void SetStatusInfo(string newStatusInfo);
+    }
+}
