7f4f4bb71f8ea8f5878487c004bf5a197475a48d
lukan-pan <lukan@xknife.net>
2014-01-24 18:02:46 +0800

    修改版本信息

--- NKnife/Properties/AssemblyInfo.cs
+++ NKnife/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过下列属性集
 // 控制。更改这些属性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Gean.Library")]
+[assembly: AssemblyTitle("NKnife")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("GeanSoft Studio.")]
-[assembly: AssemblyProduct("Gean.Library")]
-[assembly: AssemblyCopyright("Copyright © GeanSoft 2010")]
+[assembly: AssemblyCompany("xknfie.net")]
+[assembly: AssemblyProduct("NKnife")]
+[assembly: AssemblyCopyright("Copyright © xknfie.net 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyVersion("2.9.*")]
