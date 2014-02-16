3f6c51ba0ed6adfce5c9acb015f1fb18caf9c4b4
huangyang-home <huangyang@jeelu.com>
2014-02-16 20:46:34 +0800

    minnor change in DI

--- NKnife/Ioc/DI.cs
+++ NKnife/Ioc/DI.cs
@@ -1,28 +1,40 @@
 ﻿using System;
 using Ninject;
+using Ninject.Modules;
 using NKnife.Utility.File;
 
 namespace NKnife.Ioc
 {
     public static class DI
     {
+        private static bool _IsInitialized;
         private static CoreKernel _CoreKernel;
 
         public static T Get<T>()
         {
+            Initialize();
             return _CoreKernel.Get<T>();
         }
 
         public static T Get<T>(string name)
         {
+            Initialize();
             return _CoreKernel.Get<T>(name);
         }
 
         public static void Initialize()
         {
+            if (_IsInitialized) return;
             _CoreKernel = new CoreKernel();
             var assems = UtilityFile.SearchAssemblyByDirectory(AppDomain.CurrentDomain.BaseDirectory);
             _CoreKernel.Load(assems);
+            _IsInitialized = true;
+        }
+
+        public static void AddModule(NinjectModule module)
+        {
+            Initialize();
+            _CoreKernel.Load(module);
         }
 
         private class CoreKernel : StandardKernel
@@ -32,5 +44,7 @@ namespace NKnife.Ioc
             {
             }
         }
+
+
     }
 }
