b4253e2b46bee2dcc3e5111406ace550a70b801a
Erian-Lu <lukan@xknife.net>
2014-07-08 22:03:05 +0800

    再试试

--- NKnife.SocketClientTestTool/Program.cs
+++ NKnife.SocketClientTestTool/Program.cs
@@ -13,6 +13,7 @@ namespace NKnife.SocketClientTestTool
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
