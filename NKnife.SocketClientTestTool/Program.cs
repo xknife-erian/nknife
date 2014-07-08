f619b8fd1f613cd359f2867ca3553f6665424655
Erian-Lu <lukan@xknife.net>
2014-07-08 22:04:37 +0800

    再试试

--- NKnife.SocketClientTestTool/Program.cs
+++ NKnife.SocketClientTestTool/Program.cs
@@ -17,6 +17,7 @@ namespace NKnife.SocketClientTestTool
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
         }
     }
 }
