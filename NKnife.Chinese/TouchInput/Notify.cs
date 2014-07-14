0b24d9d8e9f184e0ed53809d9e7e897ab0dba796
Erian-Lu <lukan@xknife.net>
2014-07-14 22:57:32 +0800

    托盘图标功能进展。

--- NKnife.Chinese/TouchInput/Notify.cs
+++ NKnife.Chinese/TouchInput/Notify.cs
@@ -1,35 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace NKnife.Chinese.TouchInput
 {
-    class Notify
+    internal class Notify
     {
         private readonly NotifyIcon _NotifyIcon;
 
         public Notify()
         {
-            var menu = new ContextMenuStrip();
-            menu.Items.Add(_ShowMenuItem);
-            menu.Items.Add(_ExitMenu);
-
             _NotifyIcon = new NotifyIcon();
-            _NotifyIcon.ContextMenuStrip = new ContextMenuStrip();
+            _NotifyIcon.ContextMenuStrip = BuildContextMenuStrip();
             _NotifyIcon.Icon = Properties.Resources.Main;
             _NotifyIcon.MouseClick += _NotifyIcon_MouseClick;
-
         }
 
-        void _NotifyIcon_MouseClick(object sender, MouseEventArgs e)
+        private static ContextMenuStrip BuildContextMenuStrip()
+        {
+            ToolStripItem hide = new ToolStripMenuItem("隐藏");
+            hide.Click += delegate { };
+
+            ToolStripItem a = new ToolStripMenuItem("中文拼音");
+            hide.Click += delegate { };
+
+            ToolStripItem b = new ToolStripMenuItem("手写");
+            hide.Click += delegate { };
+
+            ToolStripItem c = new ToolStripMenuItem("大写英语");
+            hide.Click += delegate { };
+
+
+            ToolStripItem logger = new ToolStripMenuItem("日志");
+            logger.Click += delegate { };
+
+            ToolStripItem exit = new ToolStripMenuItem("退出");
+            exit.Click += delegate { };
+
+
+            var menu = new ContextMenuStrip();
+            menu.Items.Add(hide);
+            menu.Items.Add(new ToolStripSeparator());
+            menu.Items.Add(a);
+            menu.Items.Add(b);
+            menu.Items.Add(c);
+            menu.Items.Add(new ToolStripSeparator());
+            menu.Items.Add(logger);
+            menu.Items.Add(new ToolStripSeparator());
+            menu.Items.Add(exit);
+            return menu;
+        }
+
+        private void _NotifyIcon_MouseClick(object sender, MouseEventArgs e)
         {
             _NotifyIcon.ContextMenuStrip.Show();
         }
 
-        private readonly ToolStripItem _ShowMenuItem = new ToolStripMenuItem("显示窗体");
-        private readonly ToolStripItem _ExitMenu = new ToolStripMenuItem("退出");
 
         public void Show()
         {
@@ -40,7 +64,5 @@ namespace NKnife.Chinese.TouchInput
         {
             _NotifyIcon.Visible = false;
         }
-
-
     }
 }
