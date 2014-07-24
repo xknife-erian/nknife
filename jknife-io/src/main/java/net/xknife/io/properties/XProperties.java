ef6781a2a16b1f3638743ae651339ed47aa79e63
cripps <yangjuntao@xknife.net>
2014-07-24 11:47:09 +0800

    增加properties文件助手

---
+++ jknife-io/src/main/java/net/xknife/io/properties/XProperties.java
@@ -0,0 +1,48 @@
+package net.xknife.io.properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+/**
+ * Properties文件的助手类
+ *
+ * 【继续维护中......】
+ *
+ * Created by yangjuntao@p-an.com on 2014-07-24.
+ */
+public class XProperties
+{
+    private static final Logger _Logger = LoggerFactory.getLogger(XProperties.class);
+
+    /**
+     * 读取*.properties文件
+     *
+     * @param propertiesPath
+     * @return
+     */
+    public static Properties load(String propertiesPath)
+    {
+        Properties properties = new Properties();
+        try
+        {
+            properties.load(new FileInputStream(new File(propertiesPath)));
+        } catch (IOException e)
+        {
+            _Logger.error("读取文件错误，请检查！" + propertiesPath, e);
+            return null;
+        }
+        Enumeration keys = properties.propertyNames();
+        while (keys.hasMoreElements())
+        {
+            String key = keys.nextElement().toString();
+            properties.setProperty(key, properties.getProperty(key));
+        }
+        return properties;
+    }
+}
