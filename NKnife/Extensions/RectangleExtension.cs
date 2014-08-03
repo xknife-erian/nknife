7ae0fae9a6be3cc8b33578f9d87c788139f4f879
Erian-Lu <lukan@xknife.net>
2014-08-04 01:03:01 +0800

    RectangleExtension中增加RectangleF类型的扩展

--- NKnife/Extensions/RectangleExtension.cs
+++ NKnife/Extensions/RectangleExtension.cs
@@ -10,6 +10,14 @@ namespace System.Drawing
 {
     public static class RectangleExtension
     {
+        public static void FillXmlElement(this RectangleF rectangle, XmlElement element)
+        {
+            element.SetAttribute("left", rectangle.X.ToString());
+            element.SetAttribute("top", rectangle.Y.ToString());
+            element.SetAttribute("width", rectangle.Width.ToString());
+            element.SetAttribute("height", rectangle.Height.ToString());
+        }
+
         public static void FillXmlElement(this Rectangle rectangle, XmlElement element)
         {
             element.SetAttribute("left", rectangle.X.ToString());
