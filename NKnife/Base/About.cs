c8121bcc3409ce08e57662e9a61e54943e4881ed
p-an-erianlu <lukan@xknife.net>
2014-08-08 18:06:11 +0800

    小修正

--- NKnife/Base/About.cs
+++ NKnife/Base/About.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
 
 namespace NKnife.Base
@@ -22,9 +23,9 @@ namespace NKnife.Base
             }
         }
 
-        public static string AssemblyVersion
+        public static Version AssemblyVersion
         {
-            get { return Assembly.GetEntryAssembly().GetName().Version.ToString(); }
+            get { return Assembly.GetEntryAssembly().GetName().Version; }
         }
 
         public static string AssemblyDescription
