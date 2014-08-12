12de8a6c281b04378544ba28e69355cc82b1185c
p-an-erianlu <lukan@xknife.net>
2014-08-12 14:26:18 +0800

    小修正

---
+++ NKnife.App/NKnife.App.SocketKit/Base/ClientKit.cs
@@ -0,0 +1,30 @@
+﻿using NKnife.Ioc;
+using SocketKnife;
+using SocketKnife.Common;
+using SocketKnife.Interfaces;
+using SocketKnife.Interfaces.Sockets;
+using SocketKnife.Protocol;
+
+namespace NKnife.Socket.StarterKit.Base
+{
+    public class ClientKit : LightClient
+    {
+        public ClientKit()
+            : base(SocketMode.AsyncKeepAlive, "Socket-Client-StarterKit")
+        {
+        }
+
+        /// <summary>是否采用抽象基类中的重连机制(较简单:在有动作触发时，发现连接断开时，按频率尝试重连服务器)
+        /// </summary>
+        public override bool IsCustomTryConnectionMode
+        {
+            get { return false; }
+        }
+
+        public override ISocketClientSetting Option
+        {
+            get { return DI.Get<SocketOption>(); }
+        }
+
+    }
+}
---
+++ NKnife.App/NKnife.App.SocketKit/Ioc/ClientKitSettingModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NKnife.Socket.StarterKit.Base;
+using SocketKnife.Config;
+
+namespace NKnife.Socket.StarterKit.Ioc
+{
+    public class ClientKitSettingModule : ClientSetting
+    {
+        protected override Type GetBindType()
+        {
+            return typeof (SocketOption);
+        }
+    }
+}
