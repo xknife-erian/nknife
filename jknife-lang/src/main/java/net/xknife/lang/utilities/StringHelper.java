f7406aee70d32b431f51edb32baffa334a5ed60a
cripps <yangjuntao@xknife.net>
2014-08-28 14:57:52 +0800

    enhance StringHelper class : 1.isHanzi; 2.isMessyCode; 3.isDouble; 4.isNumber; 5.isNumberic;

--- jknife-lang/src/main/java/net/xknife/lang/utilities/StringHelper.java
+++ jknife-lang/src/main/java/net/xknife/lang/utilities/StringHelper.java
@@ -1,11 +1,12 @@
 package net.xknife.lang.utilities;
 
+import com.google.common.base.Strings;
+
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import com.google.common.base.Strings;
 
 /**
  * 字符串操作的帮助函数
@@ -703,4 +704,92 @@ public abstract class StringHelper
 		}
 		return sb.toString();
 	}
+
+    /**
+     * 判断是否为汉字
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isHanZi(String str) {
+        char[] chars = str.toCharArray();
+        boolean isGBK = false;
+        for (int i = 0; i < chars.length; i++) {
+            byte[] bytes = ("" + chars[i]).getBytes();
+            if (bytes.length == 2) {
+                int[] ints = new int[2];
+                ints[0] = bytes[0] & 0xff;
+                ints[1] = bytes[1] & 0xff;
+                if (ints[0] >= 0x81 && ints[0] <= 0xFE && ints[1] >= 0x40 && ints[1] <= 0xFE) {
+                    isGBK = true;
+                    break;
+                }
+            }
+        }
+        return isGBK;
+    }
+
+    /**
+     * 判断是否为乱码
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isMessyCode(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            // 当从Unicode编码向某个字符集转换时，如果在该字符集中没有对应的编码，则得到0x3f（即问号字符?）
+            // 从其他字符集向Unicode编码转换时，如果这个二进制数在该字符集中没有标识任何的字符，则得到的结果是0xfffd
+            // System.out.println("--- " + (int) c);
+            if ((int) c == 0xfffd) {
+                //System.out.println("存在乱码 " + (int) c);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判断字符串是否为双整型数字
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isDouble(String str) {
+        if (Strings.isNullOrEmpty(str)) {
+            return false;
+        }
+        Pattern p = Pattern.compile("-*\\d*.\\d*");
+        // Pattern p = Pattern.compile("-*"+"\\d*"+"."+"\\d*");
+        return p.matcher(str).matches();
+    }
+    /**
+     * 判断字符串是否为整字
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isNumber(String str) {
+        if (Strings.isNullOrEmpty(str)) {
+            return false;
+        }
+        Pattern p = Pattern.compile("-*\\d*");
+        return p.matcher(str).matches();
+    }
+
+    /**
+     * 判断是否为数字
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isNumeric(String str)
+    {
+        Pattern pattern = Pattern.compile("[0-9]*");
+        Matcher isNum = pattern.matcher(str);
+        if( !isNum.matches() ) {
+            return false;
+        }
+        return true;
+    }
 }
