36243c0a25cde3c43c919e6f5db173f6926f8d99
p-an-erianlu <lukan@xknife.net>
2014-09-05 16:28:28 +0800

    优化Zip的细节，小改进。

--- NKnife.Compress/AblyZip.cs
+++ NKnife.Compress/AblyZip.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using ICSharpCode.SharpZipLib.Zip;
-using NKnife.Utility.File;
 
 namespace NKnife.Compress
 {
     /// <summary>
-    /// 对SharpZipLib的使用进行了一些易用性封装
+    ///     对SharpZipLib的使用进行了一些易用性封装
     /// </summary>
-    public class AblyZip : IDisposable
+    public class AblyZip : IDisposable, IEnumerable
     {
         private readonly bool _Disposed;
         private ZipFile _SharpZipFile;
@@ -80,7 +81,7 @@ namespace NKnife.Compress
             }
         }
 
-        public ZipFile ZipFile
+        internal ZipFile ZipFile
         {
             get
             {
@@ -99,6 +100,11 @@ namespace NKnife.Compress
         }
 
         #endregion
+
+        public IEnumerator GetEnumerator()
+        {
+            return ZipFile.GetEnumerator();
+        }
 
         public void Add(string directoryName)
         {
@@ -163,105 +169,6 @@ namespace NKnife.Compress
                     _SharpZipFile.GetEntry(fileName).IsFile);
         }
 
-        /// <summary>将指定的文件集合压缩为指定的路径下的文件
-        /// </summary>
-        /// <param name="fileList">指定的文件集合.</param>
-        /// <param name="outputPath">指定的路径下的文件，含完整的路径与文件名</param>
-        /// <param name="inpath">
-        /// 压缩包中的文件的相对路径。
-        /// 可以为Null和空值，当为Null或空值时，将删除文件的所有路径信息，解压时文件将全部在一个目录中。
-        /// </param>
-        /// <param name="level">The level.</param>
-        /// <param name="password">The password.</param>
-        public static void ZipFiles(string[] fileList, string outputPath, string inpath, int level = 6, string password = "")
-        {
-            var outZipStream = new ZipOutputStream(File.Create(outputPath)); //创建Zip流
-            if (!string.IsNullOrEmpty(password))
-                outZipStream.Password = password;
-            if (level < 0 || level > 9)
-                level = 5;
-            outZipStream.SetLevel(level); //设置压缩等级
-            foreach (string file in fileList)
-            {
-                if (!File.Exists(file) || file.EndsWith(@"/")) //如果文件不存在，或者表达的是目录
-                    continue;
-                string newfilename = null;
-                if (string.IsNullOrEmpty(inpath))
-                {
-                    newfilename = file.Contains("\\") ? file.Substring(file.LastIndexOf('\\') + 1) : file;
-                }
-                else
-                {
-                    if (file.Contains(inpath))
-                    {
-                        newfilename = file.Replace(inpath, "");
-                        if (newfilename.StartsWith(@"\"))
-                            newfilename = newfilename.TrimStart('\\');
-                    }
-                }
-                var oZipEntry = new ZipEntry(newfilename)
-                                    {
-                                        Size = new FileInfo(file).Length,
-                                        IsUnicodeText = true
-                                    };
-                outZipStream.PutNextEntry(oZipEntry);
-
-                using (FileStream ostream = File.OpenRead(file))
-                {
-                    var obuffer = new byte[ostream.Length];
-                    ostream.Read(obuffer, 0, obuffer.Length);
-                    outZipStream.Write(obuffer, 0, obuffer.Length);
-                    ostream.Close();
-                }
-            }
-            outZipStream.Finish();
-            outZipStream.Close();
-        }
-
-        public static void UnZipFiles(FileInfo zipPathAndFile, string outputFolder, string password = "", bool needDeleteZipFile = false)
-        {
-            UnZipFiles(zipPathAndFile.FullName, outputFolder, password, needDeleteZipFile);
-        }
-
-        public static void UnZipFiles(string zipPathAndFile, string outputFolder, string password = "", bool needDeleteZipFile = false)
-        {
-            var zipStream = new ZipInputStream(File.OpenRead(zipPathAndFile));
-            if (!string.IsNullOrWhiteSpace(password))
-                zipStream.Password = password;
-            ZipEntry zipEntry;
-            while ((zipEntry = zipStream.GetNextEntry()) != null)
-            {
-                string fileName = Path.GetFileName(zipEntry.Name);
-                if (string.IsNullOrWhiteSpace(fileName))
-                    continue;
-                if (!string.IsNullOrWhiteSpace(outputFolder))
-                    UtilityFile.CreateDirectory(outputFolder);
-                string fullPath = outputFolder + "\\" + zipEntry.Name;
-                fullPath = fullPath.Replace("\\ ", "\\");
-                string fullDirPath = Path.GetDirectoryName(fullPath);
-                if (fullDirPath != null && !Directory.Exists(fullDirPath))
-                    UtilityFile.CreateDirectory(fullDirPath);
-                using (FileStream fileStream = new FileStream(fullPath,FileMode.OpenOrCreate))
-                {
-                    int size = 2048;
-                    var data = new byte[size];
-                    while (true)
-                    {
-                        size = zipStream.Read(data, 0, data.Length);
-                        if (size > 0)
-                            fileStream.Write(data, 0, size);
-                        else
-                            break;
-                    }
-                    fileStream.Flush();
-                    fileStream.Close();
-                }
-            }
-            zipStream.Close();
-            if (needDeleteZipFile)
-                File.Delete(zipPathAndFile);
-        }
-
         #region Nested type: StaticDataSource
 
         private class StaticDataSource : IStaticDataSource
@@ -284,5 +191,6 @@ namespace NKnife.Compress
         }
 
         #endregion
+
     }
 }
--- NKnife.Compress/ZipUtil.cs
+++ NKnife.Compress/ZipUtil.cs
@@ -1,123 +1,112 @@
-﻿using System;
-using System.Collections;
-using System.IO;
+﻿using System.IO;
 using ICSharpCode.SharpZipLib.Zip;
+using NKnife.Utility.File;
 
 namespace NKnife.Compress
 {
     public static class ZipUtil
     {
-        public static void ZipFiles(string inputFolderPath, string outputPathAndFile, string password)
+        /// <summary>
+        ///     将指定的文件集合压缩为指定的路径下的文件
+        /// </summary>
+        /// <param name="fileList">指定的文件集合.</param>
+        /// <param name="outputPath">指定的路径下的文件，含完整的路径与文件名</param>
+        /// <param name="inpath">
+        ///     压缩包中的文件的相对路径。
+        ///     可以为Null和空值，当为Null或空值时，将删除文件的所有路径信息，解压时文件将全部在一个目录中。
+        /// </param>
+        /// <param name="level">The level.</param>
+        /// <param name="password">The password.</param>
+        public static void ZipFiles(string[] fileList, string outputPath, string inpath, int level = 6,
+            string password = "")
         {
-            ArrayList ar = GenerateFileList(inputFolderPath); // generate file list
-            int trimLength = (Directory.GetParent(inputFolderPath)).ToString().Length;
-            // find number of chars to remove     // from orginal file path
-            trimLength += 1; //remove '\'
-            FileStream ostream;
-            byte[] obuffer;
-            string outPath = outputPathAndFile;
-            ZipOutputStream oZipStream = new ZipOutputStream(File.Create(outPath)); // create zip stream
-            if (password != null && password != String.Empty)
-                oZipStream.Password = password;
-            oZipStream.SetLevel(9); // maximum compression
-            ZipEntry oZipEntry;
-            foreach (string Fil in ar) // for each file, generate a zipentry
+            var outZipStream = new ZipOutputStream(File.Create(outputPath)); //创建Zip流
+            if (!string.IsNullOrEmpty(password))
+                outZipStream.Password = password;
+            if (level < 0 || level > 9)
+                level = 5;
+            outZipStream.SetLevel(level); //设置压缩等级
+            foreach (string file in fileList)
             {
-                oZipEntry = new ZipEntry(Fil.Remove(0, trimLength));
-                oZipEntry.Size = new FileInfo(Fil).Length;
-                oZipEntry.IsUnicodeText = true;
-                oZipStream.PutNextEntry(oZipEntry);
-
-                if (!Fil.EndsWith(@"/")) // if a file ends with '/' its a directory
+                if (!File.Exists(file) || file.EndsWith(@"/")) //如果文件不存在，或者表达的是目录
+                    continue;
+                string newfilename = null;
+                if (string.IsNullOrEmpty(inpath))
                 {
-                    ostream = File.OpenRead(Fil);
-                    obuffer = new byte[ostream.Length];
-                    ostream.Read(obuffer, 0, obuffer.Length);
-                    oZipStream.Write(obuffer, 0, obuffer.Length);
+                    newfilename = file.Contains("\\") ? file.Substring(file.LastIndexOf('\\') + 1) : file;
                 }
-            }
-            oZipStream.Finish();
-            oZipStream.Close();
-        }
-
-
-        private static ArrayList GenerateFileList(string Dir)
-        {
-            ArrayList fils = new ArrayList();
-            bool Empty = true;
-            foreach (string file in Directory.GetFiles(Dir)) // add each file in directory
-            {
-                fils.Add(file);
-                Empty = false;
-            }
-
-            if (Empty)
-            {
-                if (Directory.GetDirectories(Dir).Length == 0)
-                // if directory is completely empty, add it
+                else
                 {
-                    fils.Add(Dir + @"/");
-                }
-            }
-
-            foreach (string dirs in Directory.GetDirectories(Dir)) // recursive
-            {
-                foreach (object obj in GenerateFileList(dirs))
-                {
-                    fils.Add(obj);
-                }
-            }
-            return fils; // return file list
-        }
-
-
-        public static void UnZipFiles(string zipPathAndFile, string outputFolder, string password, bool deleteZipFile)
-        {
-            ZipInputStream s = new ZipInputStream(File.OpenRead(zipPathAndFile));
-            if (password != null && password != String.Empty)
-                s.Password = password;
-            ZipEntry theEntry;
-            string tmpEntry = String.Empty;
-            while ((theEntry = s.GetNextEntry()) != null)
-            {
-                string directoryName = outputFolder;
-                string fileName = Path.GetFileName(theEntry.Name);
-                // create directory 
-                if (directoryName != "")
-                {
-                    Directory.CreateDirectory(directoryName);
-                }
-                if (fileName != String.Empty)
-                {
-                    if (theEntry.Name.IndexOf(".ini") < 0)
+                    if (file.Contains(inpath))
                     {
-                        string fullPath = directoryName + "\\" + theEntry.Name;
-                        fullPath = fullPath.Replace("\\ ", "\\");
-                        string fullDirPath = Path.GetDirectoryName(fullPath);
-                        if (!Directory.Exists(fullDirPath)) Directory.CreateDirectory(fullDirPath);
-                        FileStream streamWriter = File.Create(fullPath);
-                        int size = 2048;
-                        byte[] data = new byte[2048];
-                        while (true)
-                        {
-                            size = s.Read(data, 0, data.Length);
-                            if (size > 0)
-                            {
-                                streamWriter.Write(data, 0, size);
-                            }
-                            else
-                            {
-                                break;
-                            }
-                        }
-                        streamWriter.Close();
+                        newfilename = file.Replace(inpath, "");
+                        if (newfilename.StartsWith(@"\"))
+                            newfilename = newfilename.TrimStart('\\');
                     }
                 }
+                var oZipEntry = new ZipEntry(newfilename)
+                {
+                    Size = new FileInfo(file).Length,
+                    IsUnicodeText = true
+                };
+                outZipStream.PutNextEntry(oZipEntry);
+
+                using (FileStream ostream = File.OpenRead(file))
+                {
+                    var obuffer = new byte[ostream.Length];
+                    ostream.Read(obuffer, 0, obuffer.Length);
+                    outZipStream.Write(obuffer, 0, obuffer.Length);
+                    ostream.Close();
+                }
             }
-            s.Close();
-            if (deleteZipFile)
+            outZipStream.Finish();
+            outZipStream.Close();
+        }
+
+        public static void UnZipFiles(FileInfo zipPathAndFile, string outputFolder, string password = "",
+            bool needDeleteZipFile = false)
+        {
+            UnZipFiles(zipPathAndFile.FullName, outputFolder, password, needDeleteZipFile);
+        }
+
+        public static void UnZipFiles(string zipPathAndFile, string outputFolder, string password = "",
+            bool needDeleteZipFile = false)
+        {
+            var zipStream = new ZipInputStream(File.OpenRead(zipPathAndFile));
+            if (!string.IsNullOrWhiteSpace(password))
+                zipStream.Password = password;
+            ZipEntry zipEntry;
+            while ((zipEntry = zipStream.GetNextEntry()) != null)
+            {
+                string fileName = Path.GetFileName(zipEntry.Name);
+                if (string.IsNullOrWhiteSpace(fileName))
+                    continue;
+                if (!string.IsNullOrWhiteSpace(outputFolder))
+                    UtilityFile.CreateDirectory(outputFolder);
+                string fullPath = outputFolder + "\\" + zipEntry.Name;
+                fullPath = fullPath.Replace("\\ ", "\\");
+                string fullDirPath = Path.GetDirectoryName(fullPath);
+                if (fullDirPath != null && !Directory.Exists(fullDirPath))
+                    UtilityFile.CreateDirectory(fullDirPath);
+                using (var fileStream = new FileStream(fullPath, FileMode.OpenOrCreate))
+                {
+                    int size = 2048;
+                    var data = new byte[size];
+                    while (true)
+                    {
+                        size = zipStream.Read(data, 0, data.Length);
+                        if (size > 0)
+                            fileStream.Write(data, 0, size);
+                        else
+                            break;
+                    }
+                    fileStream.Flush();
+                    fileStream.Close();
+                }
+            }
+            zipStream.Close();
+            if (needDeleteZipFile)
                 File.Delete(zipPathAndFile);
         }
-   
     }
 }
