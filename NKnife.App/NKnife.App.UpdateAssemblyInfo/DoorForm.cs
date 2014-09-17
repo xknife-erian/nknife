813a312506f344d5e705420a6a05eebf6c47a76a
p-an-erianlu <lukan@xknife.net>
2014-09-17 12:11:59 +0800

    开发中

--- NKnife.App/NKnife.App.UpdateAssemblyInfo/DoorForm.cs
+++ NKnife.App/NKnife.App.UpdateAssemblyInfo/DoorForm.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace NKnife.App.UpdateAssemblyInfo
 {
-    public partial class Form1 : Form
+    public partial class DoorForm : Form
     {
-        public Form1()
+        public DoorForm()
         {
             InitializeComponent();
         }
--- NKnife.App/NKnife.App.UpdateAssemblyInfo/Program.cs
+++ NKnife.App/NKnife.App.UpdateAssemblyInfo/Program.cs
@@ -15,7 +15,7 @@ namespace NKnife.App.UpdateAssemblyInfo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DoorForm());
         }
     }
 }
