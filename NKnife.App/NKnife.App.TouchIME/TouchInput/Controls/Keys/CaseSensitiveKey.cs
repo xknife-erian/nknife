d14222c18d45f4ce14889a4a76e19fc49d25100f
p-an-erianlu <lukan@xknife.net>
2014-09-18 15:07:40 +0800

    屏幕输入法独立研发为应用程序。将部份中文相关的帮助类移入到主库中。

---
+++ NKnife.App/NKnife.App.TouchIME/TouchInput/Controls/Keys/CaseSensitiveKey.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using WindowsInput.Native;
+
+namespace NKnife.App.TouchIme.TouchInput.Controls.Keys
+{
+    public class CaseSensitiveKey : MultiCharacterKey
+    {
+        public CaseSensitiveKey(VirtualKeyCode keyCode, IList<string> keyDisplays)
+            : base(keyCode, keyDisplays)
+        {
+        }
+    }
+}
---
+++ NKnife.App/NKnife.App.TouchIME/TouchInput/Controls/Keys/ShiftSensitiveKey.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using WindowsInput.Native;
+
+namespace NKnife.App.TouchIme.TouchInput.Controls.Keys
+{
+    public class ShiftSensitiveKey : MultiCharacterKey
+    {
+        public ShiftSensitiveKey(VirtualKeyCode keyCode, IList<string> keyDisplays)
+            : base(keyCode, keyDisplays)
+        {
+        }
+    }
+}
