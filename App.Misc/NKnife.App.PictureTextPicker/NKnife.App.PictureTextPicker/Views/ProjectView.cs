665441a782a0c93fc81493318568a3258880d6c5
huangyang-home <huangyang@jeelu.com>
2014-09-21 21:53:56 +0800

    项目设计，左侧是图片列表，右侧上方图片预览，下方是图片属性及标题，工具栏操作

---
+++ App.Misc/NKnife.App.PictureTextPicker/NKnife.App.PictureTextPicker/Views/ProjectView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace NKnife.App.PictureTextPicker.Views
+{
+    public class ProjectView :DockContent
+    {
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ProjectView
+            // 
+            this.ClientSize = new System.Drawing.Size(378, 366);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "ProjectView";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
