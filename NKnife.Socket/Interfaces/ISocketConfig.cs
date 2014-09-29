8265fd05438ab75bccbc9dabc953810554c32cbd
p-an-erianlu <lukan@xknife.net>
2014-09-29 17:56:43 +0800

    研发过程中...

--- NKnife.Socket/Interfaces/ISocketConfig.cs
+++ NKnife.Socket/Interfaces/ISocketConfig.cs
@@ -2,7 +2,31 @@ namespace SocketKnife.Interfaces
 {
     public interface ISocketConfig
     {
-        void SetReadBufferSize(int size);
-        void SetIdleTime(object bothIdle, int i);
+        int ReadBufferSize { get; set; }
+
+        /// <summary>
+        ///     接收包大小
+        /// </summary>
+        int MaxBufferSize { get; set; }
+
+        /// <summary>
+        ///     最大用户连接数
+        /// </summary>
+        int MaxConnectCount { get; set; }
+
+        /// <summary>
+        ///     是否关闭SOCKET Delay算法
+        /// </summary>
+        bool NoDelay { get; set; }
+
+        /// <summary>
+        ///     SOCKET 的 ReceiveTimeout属性
+        /// </summary>
+        int ReceiveTimeout { get; set; }
+
+        /// <summary>
+        ///     SOCKET 的 SendTimeout
+        /// </summary>
+        int SendTimeout { get; set; }
     }
 }
