95cf841b7afdd9a2ac262b7f5bb52b73cc56a2b1
lukan@xknife.net <lukan@xknife.net>
2014-09-30 21:14:22 +0800

    minor...

--- App.NKnifeSocketKit/NKnife.App.SocketKit/Workbench/MainWindow.xaml.cs
+++ App.NKnifeSocketKit/NKnife.App.SocketKit/Workbench/MainWindow.xaml.cs
@@ -1,27 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using NKnife.App.SocketKit.Common;
 using NKnife.App.SocketKit.Dialogs;
 using NKnife.App.SocketKit.Views;
-using NKnife.Ioc;
 using Xceed.Wpf.AvalonDock;
 using Xceed.Wpf.AvalonDock.Layout;
 using Xceed.Wpf.AvalonDock.Themes;
 
-namespace NKnife.App.SocketKit
+namespace NKnife.App.SocketKit.Workbench
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
