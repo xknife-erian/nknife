8824c986ff2fb814902e1d50b1e1ae16cca9e0f9
p-an-erianlu <lukan@xknife.net>
2014-10-11 11:49:34 +0800

    minor change.../lukan/sx

--- NKnife/Extensions/QueryExtension.cs
+++ NKnife/Extensions/QueryExtension.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 
-namespace NKnife.Extensions
+namespace System.Data
 {
     public static class QueryExtension
     {
@@ -19,7 +18,7 @@ namespace NKnife.Extensions
         }
 
         /// <summary>
-        /// DataRow扩展方法：将DataRow类型转化为指定类型的实体
+        ///     DataRow扩展方法：将DataRow类型转化为指定类型的实体
         /// </summary>
         /// <typeparam name="T">实体类型</typeparam>
         /// <returns></returns>
@@ -27,8 +26,9 @@ namespace NKnife.Extensions
         {
             return ToModel<T>(dr, true);
         }
+
         /// <summary>
-        /// DataRow扩展方法：将DataRow类型转化为指定类型的实体
+        ///     DataRow扩展方法：将DataRow类型转化为指定类型的实体
         /// </summary>
         /// <typeparam name="T">实体类型</typeparam>
         /// <param name="dateTimeToString">是否需要将日期转换为字符串，默认为转换,值为true</param>
@@ -36,11 +36,12 @@ namespace NKnife.Extensions
         public static T ToModel<T>(this DataRow dr, bool dateTimeToString) where T : class, new()
         {
             if (dr != null)
-                return ToList<T>(dr.Table, dateTimeToString).First();
+                return ToList<T>(dr.Table, dateTimeToString).First();//TODO:此处感觉有BUG
             return null;
         }
+
         /// <summary>
-        /// DataTable扩展方法：将DataTable类型转化为指定类型的实体集合
+        ///     DataTable扩展方法：将DataTable类型转化为指定类型的实体集合
         /// </summary>
         /// <typeparam name="T">实体类型</typeparam>
         /// <returns></returns>
@@ -48,43 +49,47 @@ namespace NKnife.Extensions
         {
             return ToList<T>(dt, true);
         }
+
         /// <summary>
-        /// DataTable扩展方法：将DataTable类型转化为指定类型的实体集合
+        ///     DataTable扩展方法：将DataTable类型转化为指定类型的实体集合
         /// </summary>
         /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="dt"></param>
         /// <param name="dateTimeToString">是否需要将日期转换为字符串，默认为转换,值为true</param>
         /// <returns></returns>
         public static List<T> ToList<T>(this DataTable dt, bool dateTimeToString) where T : class, new()
         {
-            List<T> list = new List<T>();
+            var list = new List<T>();
 
             if (dt != null)
             {
-                List<PropertyInfo> infos = new List<PropertyInfo>();
-                Array.ForEach<PropertyInfo>(typeof(T).GetProperties(), p =>
+                var infos = new List<PropertyInfo>();
+                Array.ForEach(typeof (T).GetProperties(), p =>
                 {
-                    if (dt.Columns.Contains(p.Name) == true)
+                    if (dt.Columns.Contains(p.Name))
                     {
                         infos.Add(p);
                     }
                 });
-                SetList<T>(list, infos, dt, dateTimeToString);
+                SetList(list, infos, dt, dateTimeToString);
             }
             return list;
         }
-       
+
         #region 私有方法
-        private static void SetList<T>(List<T> list, List<PropertyInfo> infos, DataTable dt, bool dateTimeToString) where T : class, new()
+
+        private static void SetList<T>(ICollection<T> list, List<PropertyInfo> infos, DataTable dt, bool dateTimeToString)
+            where T : class, new()
         {
             foreach (DataRow dr in dt.Rows)
             {
-                T model = new T();
+                var model = new T();
                 infos.ForEach(p =>
                 {
                     if (dr[p.Name] != DBNull.Value)
                     {
                         object tempValue = dr[p.Name];
-                        if (dr[p.Name].GetType() == typeof(DateTime) && dateTimeToString)
+                        if (dr[p.Name] is DateTime && dateTimeToString)
                         {
                             tempValue = dr[p.Name].ToString();
                         }
@@ -92,12 +97,16 @@ namespace NKnife.Extensions
                         {
                             p.SetValue(model, tempValue, null);
                         }
-                        catch { }
+                        catch (Exception e)
+                        {
+                            Debug.Fail(e.Message);
+                        }
                     }
                 });
                 list.Add(model);
             }
         }
+
         #endregion
     }
 }
