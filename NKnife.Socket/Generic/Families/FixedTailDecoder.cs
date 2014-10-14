ba0e2ae44cfb85438bcacbfa873ec8089a39d626
p-an-erianlu <lukan@xknife.net>
2014-10-14 16:59:46 +0800

    研发过程中.../lukan/sx

--- NKnife.Socket/Generic/Families/FixedTailDecoder.cs
+++ NKnife.Socket/Generic/Families/FixedTailDecoder.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NKnife.Adapters;
+using NKnife.Interface;
 using NKnife.Utility;
 using SocketKnife.Interfaces;
 
@@ -9,26 +11,29 @@ namespace SocketKnife.Generic.Families
 {
     public class FixedTailDecoder : IDatagramDecoder
     {
-        private static readonly byte[] _tail = Encoding.Default.GetBytes("\n");
+        private static readonly byte[] _tail = Encoding.Default.GetBytes("\r\n");
 
         protected virtual byte[] GetTail()
         {
             return _tail;
         }
 
+        protected virtual string GetString(byte[] data, int index, int count)
+        {
+            return Encoding.Default.GetString(data, index, count);
+        }
+
         public string[] Execute(byte[] data, out int done)
         {
             done = data.Length;
-            //data.Contains(GetTail())
-            string src = UtilityString.TidyUTF8(data);
-            string[] result = src.Contains("ö") ? src.Split(new[] { 'ö' }, StringSplitOptions.RemoveEmptyEntries) : new[] { src };
-            if (result.Length > 1)
+            var index = data.IndexOf(_tail);
+            if (index < 0)
             {
-                for (int i = 0; i < result.Length; i++)
-                {
-                    result[i] = UtilityString.TidyUTF8(result[i]).Trim();
-                }
+                done = 0;
+                return new string[0];
             }
+            string src = GetString(data, 0, index);
+            var result = new[] {src};
             return result;
         }
     }
