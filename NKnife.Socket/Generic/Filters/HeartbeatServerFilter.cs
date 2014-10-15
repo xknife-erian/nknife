ce7d52d17d0691a145a312d763ca3853d6e457e4
p-an-erianlu <lukan@xknife.net>
2014-10-15 11:42:42 +0800

    研发过程中.../lukan/sx

---
+++ NKnife.Socket/Generic/Filters/HeartbeatServerFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using SocketKnife.Common;
+using SocketKnife.Interfaces;
+
+namespace SocketKnife.Generic.Filters
+{
+    public class HeartbeatServerFilter : KnifeSocketServerFilter
+    {
+        public override void PrcoessReceiveData(Socket socket, byte[] data)
+        {
+            
+        }
+    }
+}
