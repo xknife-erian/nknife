6b310d2d841624f0ff5ecdfae965be9b0ab0d244
p-an-erianlu <lukan@xknife.net>
2014-10-20 14:03:14 +0800

    研发过程中.../lukan/sx

--- App.NKnifeSocketKit/NKnife.App.SocketKit/Demo/DemoCodec.cs
+++ App.NKnifeSocketKit/NKnife.App.SocketKit/Demo/DemoCodec.cs
@@ -9,7 +9,7 @@ namespace NKnife.App.SocketKit.Demo
 {
     public class DemoCodec : KnifeSocketCodec
     {
-        public override ISocketCommandParser SocketCommandParser { get; set; }
+        public override KnifeSocketCommandParser SocketCommandParser { get; set; }
         public override KnifeSocketDatagramDecoder SocketDecoder { get; set; }
         public override KnifeSocketDatagramEncoder SocketEncoder { get; set; }
     }
