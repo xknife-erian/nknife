2c163afaa7a8e3ed4d75ea72c6addf11f9b29751
huangyang-home <huangyang@xknife.net>
2014-10-22 12:57:28 +0800

    minnor change
    构造函数里面对SessionMap进行初始化，防止null pointer 异常

--- NKnife.Socket/Generic/KnifeSocketServerProtocolHandler.cs
+++ NKnife.Socket/Generic/KnifeSocketServerProtocolHandler.cs
@@ -3,5 +3,10 @@ namespace SocketKnife.Generic
     public abstract class KnifeSocketServerProtocolHandler : KnifeSocketProtocolHandler
     {
         public KnifeSocketSessionMap SessionMap { get; set; }
+
+        protected KnifeSocketServerProtocolHandler()
+        {
+            SessionMap = new KnifeSocketSessionMap();
+        }
     }
 }
