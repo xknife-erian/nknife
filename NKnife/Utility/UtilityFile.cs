53c07ef9f1f56341f57947f3569c1825e5041883
p-an-erianlu <lukan@xknife.net>
2014-10-29 10:55:37 +0800

    DI在开发期查找程序集时，不知为什么会找到VS的目录中去，导到弹出断言，暂时注释掉。

--- NKnife/Utility/UtilityFile.cs
+++ NKnife/Utility/UtilityFile.cs
@@ -891,7 +891,7 @@ namespace NKnife.Utility
                     }
                     catch (Exception e)
                     {
-                        Debug.Fail(string.Format("Assembly.LoadFile导常，{0}\r\n{1}", dllPath, e.Message));
+                        //Debug.Fail(string.Format("Assembly.LoadFile导常，{0}\r\n{1}", dllPath, e.Message));
                         continue;
                     }
                 }
@@ -904,7 +904,7 @@ namespace NKnife.Utility
                     }
                     catch (Exception e)
                     {
-                        Debug.Fail(string.Format("Assembly.LoadFile导常，{0}\r\n{1}", exePath, e.Message));
+                        //Debug.Fail(string.Format("Assembly.LoadFile导常，{0}\r\n{1}", exePath, e.Message));
                         continue;
                     }
                 }
