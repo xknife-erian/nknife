a6a8d72e931bd9c9682bb691c4d8ab96b975f5c6
huangyang-home <huangyang@xknife.net>
2014-10-31 11:19:13 +0800

    通用的EventArgs`类增加了针对两个参数和三个参数的实现（原来只有针对一个参数的）

--- NKnife/Events/EventArgs`.cs
+++ NKnife/Events/EventArgs`.cs
@@ -7,7 +7,33 @@ namespace NKnife.Events
         public T Item { get; private set; }
         public EventArgs(T item)
         {
-            this.Item = item;
+            Item = item;
+        }
+    }
+
+    public class EventArgs<T1, T2> : EventArgs
+    {
+        public T1 Item1 { get; private set; }
+        public T2 Item2 { get; private set; }
+
+        public EventArgs(T1 item1, T2 item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+    }
+
+    public class EventArgs<T1, T2, T3> : EventArgs
+    {
+        public T1 Item1 { get; private set; }
+        public T2 Item2 { get; private set; }
+        public T3 Item3 { get; private set; }
+
+        public EventArgs(T1 item1, T2 item2, T3 item3)
+        {
+            Item1 = item1;
+            Item2 = item2;
+            Item3 = item3;
         }
     }
 
