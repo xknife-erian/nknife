b28544b56fa0106bdbb1fb29864be049988823a4
lukan@xknife.net <lukan@xknife.net>
2014-11-07 23:28:51 +0800

    开发过程，小阶段提交.../lukan

--- Kits/NKnife.Kits.SocketKnife/Dialogs/ProtocolEditorDialog.xaml.cs
+++ Kits/NKnife.Kits.SocketKnife/Dialogs/ProtocolEditorDialog.xaml.cs
@@ -10,7 +10,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using NKnife.Protocol;
 using NKnife.Protocol.Generic;
+using NKnife.Protocol.Generic.TextPlain;
+using NKnife.Utility;
 
 namespace NKnife.Kits.SocketKnife.Dialogs
 {
@@ -22,6 +25,20 @@ namespace NKnife.Kits.SocketKnife.Dialogs
         public ProtocolEditorDialog()
         {
             InitializeComponent();
+
+            var packers = UtilityType.FindTypesByDirectory(AppDomain.CurrentDomain.BaseDirectory, typeof(IProtocolPacker<>), true);
+            foreach (var packer in packers)
+            {
+                _PackerComboBox.Items.Add(packer);
+            }
+            _PackerComboBox.SelectedItem = typeof(TextPlainPacker);
+
+            var unpackers = UtilityType.FindTypesByDirectory(AppDomain.CurrentDomain.BaseDirectory, typeof(IProtocolUnPacker<>), true);
+            foreach (var unpacker in unpackers)
+            {
+                _UnPackerComboBox.Items.Add(unpacker);
+            }
+            _UnPackerComboBox.SelectedItem = typeof(TextPlainUnPacker);
         }
 
         public StringProtocol CurrentProtocol { get; set; }
