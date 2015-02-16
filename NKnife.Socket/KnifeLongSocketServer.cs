6b4f1449758deb2dd36b4e464897d226f836014c
huangyang <huangyang@xknife.net>
2015-02-16 15:47:43 +0800

    minnor

--- NKnife.Socket/KnifeLongSocketServer.cs
+++ NKnife.Socket/KnifeLongSocketServer.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 using Common.Logging;
 using NKnife.IoC;
