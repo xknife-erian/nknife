1a7c704948719a8e43dcb20b9b462d6051788f3d
p-an-erianlu <lukan@xknife.net>
2015-03-02 16:17:50 +0800

    研发过程中.../lukan/sx

--- Kits/NKnife.Kits.SimpleDataKit/NKnife.Kits.SimpleDataKit.Mysql/Program.cs
+++ Kits/NKnife.Kits.SimpleDataKit/NKnife.Kits.SimpleDataKit.Mysql/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,29 +17,118 @@ namespace NKnife.Kits.SimpleDataKit.Mysql
             Console.WriteLine("==Simple.Data.Mysql----------");
 
             var db = Database.OpenConnection(CONNECTION_STRING);
+            db.book.DeleteAll();
 
-            var book = db.book.FindById("a001");
+            var stopwatch = new Stopwatch();
 
-            Console.WriteLine(book.id);
-            Console.WriteLine(book.name);
-            Console.WriteLine(book.page);
+            stopwatch.Restart();
+            db.book.Insert(id: "X00001", name: string.Format("Abcdefg{0}", 777), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
 
-            for (int i = 0; i < 100; i++)
+            stopwatch.Restart();
+            db.book.Insert(id: "X00002", name: string.Format("Abcdefg{0}", 888), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00003", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00004", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00005", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00006", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00007", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00008", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00008", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00009", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            Console.WriteLine("------------");
+
+            stopwatch.Restart();
+            var book = db.book.FindById("X00005");
+            if (book != null)
+                Write(book);
+            else
+                Console.WriteLine("Has error! Don't find target record!");
+            stopwatch.Stop();
+            Console.WriteLine("1 record found! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            book = db.book.FindById("X00001");
+            if (book != null)
+                Write(book);
+            else
+                Console.WriteLine("Has error! Don't find target record!");
+            stopwatch.Stop();
+            Console.WriteLine("1 record found! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            book = db.book.FindById("X00009");
+            if (book != null)
+                Write(book);
+            else
+                Console.WriteLine("Has error! Don't find target record!");
+            stopwatch.Stop();
+            Console.WriteLine("1 record found! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            book = db.book.FindById("X00004");
+            if (book != null)
+                Write(book);
+            else
+                Console.WriteLine("Has error! Don't find target record!");
+            stopwatch.Stop();
+            Console.WriteLine("1 record found! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            Console.WriteLine("------------");
+
+            stopwatch.Restart();
+            int count = 500;
+            for (int i = 0; i < count; i++)
             {
-                var length = db.book.GetCount();
-                Console.WriteLine(length);
-
-                var bookid = string.Format("a{0}", length.ToString().PadLeft(5, '0'));
-                db.book.Insert(id: bookid, name: string.Format("Steve{0}", length), page: 50);
-
-                book = db.book.FindById(bookid);
-
-                Console.WriteLine(book.id);
-                Console.WriteLine(book.name);
-                Console.WriteLine(book.page);
+                var bookid = string.Format("A{0}", i.ToString().PadLeft(5, '0'));
+                db.book.Insert(id: bookid, name: string.Format("Steve{0}", i), page: 50);
+                Console.Write('.');
             }
+            stopwatch.Stop();
+            Console.WriteLine();
+            Console.WriteLine("{1} record created! process {0}ms.", stopwatch.ElapsedMilliseconds, count);
 
             Console.ReadKey();
+        }
+
+        private static void Write(dynamic book)
+        {
+            Console.WriteLine(string.Format("[{0}],{1},{2}", book.id, book.name, book.page));
         }
     }
 }
--- NKnife.Kits.SimpleDataKit.Sqlite/Program.cs
+++ NKnife.Kits.SimpleDataKit.Sqlite/Program.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Data.SQLite;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using Simple.Data;
 
@@ -10,43 +13,147 @@ namespace NKnife.Kits.SimpleDataKit.Sqlite
 {
     class Program
     {
-        private const string CONNECTION_STRING = @"Data Source=d:\simple-data-books.db;Version=3;";
+        private const string FILE = @"d:\simple-data-books.db";
+        private static readonly string _connectionString = string.Format("Data Source={0};Version=3;", FILE);
 
         private static void Main(string[] args)
         {
             Console.WriteLine("==Simple.Data.Sqlite----------");
+            if (File.Exists(FILE))
+            {
+                File.Delete(FILE);
+            }
 
-            var db = Database.OpenConnection(CONNECTION_STRING);
-            SQLiteConnection conn = new SQLiteConnection(CONNECTION_STRING);
+            var db = Database.OpenConnection(_connectionString);
+            SQLiteConnection conn = new SQLiteConnection(_connectionString);
             conn.Open();
             db.UseSharedConnection(conn);
 
-            //SELECT COUNT(*) FROM sqlite_master where type='table' and name='Student';
-
-            var isExist = db.sqlite_master.FindByTypeAndName("table", "book");
-
-            var book = db.book.FindById("a001");
-
-            Console.WriteLine(book.id);
-            Console.WriteLine(book.name);
-            Console.WriteLine(book.page);
-
-            for (int i = 0; i < 100; i++)
+            using (SQLiteCommand cmd = new SQLiteCommand())
             {
-                var length = db.book.GetCount();
-                Console.WriteLine(length);
+                cmd.Connection = conn;
+                cmd.CommandText = "";
+                SQLiteHelper helper = new SQLiteHelper(cmd);
 
-                var bookid = string.Format("a{0}", length.ToString().PadLeft(5, '0'));
-                db.book.Insert(id: bookid, name: string.Format("Steve{0}", length), page: 50);
+                SQLiteTable tb = new SQLiteTable("book");
 
-                book = db.book.FindById(bookid);
+                tb.Columns.Add(new SQLiteColumn("id"));
+                tb.Columns.Add(new SQLiteColumn("name"));
+                tb.Columns.Add(new SQLiteColumn("page", ColType.Integer));
 
-                Console.WriteLine(book.id);
-                Console.WriteLine(book.name);
-                Console.WriteLine(book.page);
+                helper.CreateTable(tb);
+                Console.WriteLine("Table:books is created!");
             }
 
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00001", name: string.Format("Abcdefg{0}", 777), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00002", name: string.Format("Abcdefg{0}", 888), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00003", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00004", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00005", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00006", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00007", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00008", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00008", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            db.book.Insert(id: "X00009", name: string.Format("Abcdefg{0}", 999), page: 50);
+            stopwatch.Stop();
+            Console.WriteLine("1 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            Console.WriteLine("------------");
+
+            stopwatch.Restart();
+            var book = db.book.FindById("X00005");
+            if (book != null)
+                Write(book);
+            else
+                Console.WriteLine("Has error! Don't find target record!");
+            stopwatch.Stop();
+            Console.WriteLine("1 record found! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            book = db.book.FindById("X00001");
+            if (book != null)
+                Write(book);
+            else
+                Console.WriteLine("Has error! Don't find target record!");
+            stopwatch.Stop();
+            Console.WriteLine("1 record found! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            book = db.book.FindById("X00009");
+            if (book != null)
+                Write(book);
+            else
+                Console.WriteLine("Has error! Don't find target record!");
+            stopwatch.Stop();
+            Console.WriteLine("1 record found! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            book = db.book.FindById("X00004");
+            if (book != null)
+                Write(book);
+            else
+                Console.WriteLine("Has error! Don't find target record!");
+            stopwatch.Stop();
+            Console.WriteLine("1 record found! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
+            Console.WriteLine("------------");
+
+            stopwatch.Restart();
+            for (int i = 0; i < 50; i++)
+            {
+                var bookid = string.Format("A{0}", i.ToString().PadLeft(5, '0'));
+                db.book.Insert(id: bookid, name: string.Format("Steve{0}", i), page: 50);
+                Console.Write('.');
+            }
+            stopwatch.Stop();
+            Console.WriteLine();
+            Console.WriteLine("50 record created! process {0}ms.", stopwatch.ElapsedMilliseconds);
+
             Console.ReadKey();
+        }
+
+        private static void Write(dynamic book)
+        {
+            Console.WriteLine(string.Format("[{0}],{1},{2}", book.id, book.name, book.page));
         }
     }
 }
