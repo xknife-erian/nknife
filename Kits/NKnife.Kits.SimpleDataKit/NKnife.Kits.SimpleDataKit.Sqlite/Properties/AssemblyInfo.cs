96c1ddd2c8849f4086f3b94384d5233f63e20e5b
p-an-erianlu <lukan@xknife.net>
2015-03-17 15:50:41 +0800

    minor change.../lukan/sx

--- Kits/NKnife.Kits.SimpleDataKit/NKnife.Kits.SimpleDataKit.Sqlite/Properties/AssemblyInfo.cs
+++ Kits/NKnife.Kits.SimpleDataKit/NKnife.Kits.SimpleDataKit.Sqlite/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("104d205b-2676-4304-b4ba-fc68c5751156")]
+[assembly: Guid("09f2eb7d-d1c2-4376-9ae8-ce5e362e8425")]
 
 // 程序集的版本信息由下面四个值组成: 
 //
