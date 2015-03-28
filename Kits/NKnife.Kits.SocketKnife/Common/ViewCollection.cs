4315237d16cbe929badefa8780f5949fc4cdd5e2
lukan@xknife.net <lukan@xknife.net>
2015-03-28 20:57:14 +0800

    准备创建控制台的Serial和Socket测试

--- Kits/NKnife.Kits.SocketKnife/Common/ViewCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.ObjectModel;
-using Xceed.Wpf.AvalonDock.Layout;
-
-namespace NKnife.Kits.SocketKnife.Common
-{
-    public class ViewCollection : ObservableCollection<LayoutContent>
-    {
-    }
-}
