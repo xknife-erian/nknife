a0a5f163ba09378eec0513b699e4d86745b904e8
lukan@xknife.net <lukan@xknife.net>
2015-04-08 22:02:54 +0800

    开发过程，小阶段提交.../lukan

--- Kits/NKnife.Kits.TunnelKit/NKnife.Kits.SerialKnife.Consoles/Program.cs
+++ Kits/NKnife.Kits.TunnelKit/NKnife.Kits.SerialKnife.Consoles/Program.cs
@@ -20,28 +20,21 @@ namespace NKnife.Kits.SerialKnife.Consoles
 
             _logger.Info("DI初始化结束....");
 
-            var server1 = new SerialClient(7);
+            var server1 = new SerialClient(4);
             server1.Start();
 
             Thread.Sleep(100);
 
-            var count = 50;
+            var count = 3;
             Console.WriteLine("----------------");
             var sw = new Stopwatch();
             sw.Start();
             for (var i = 0; i < count; i++)
             {
-                if (i%2 == 0)
-                {
-                    server1.Send(new byte[] {0x09, 0x00, 0x05, 0xAA, 0x01, 0x31, 0x32, 0x33});
-                    Console.Write(">");
-                }
-                else
-                {
-                    server1.Send(new byte[] {0x09, 0x00, 0x05, 0xAA, 0x01, 0x41, 0x42, 0x43});//, 0x09, 0x00, 0x05, 0xAA, 0x01, 0x51, 0x52, 0x53});
-                    Console.Write(">");
-                }
-                Thread.Sleep(2);
+                //09 17 09 AA 00 52 45 41 44 3F 0D 0A
+                server1.Send(new byte[] {0x08, 0x17, 0x09, 0xAA, 0x00, 0x52, 0x45, 0x41, 0x44, 0x3F, 0x0D, 0x0A});
+                Console.Write("<<< ");
+                Thread.Sleep(50);
             }
             sw.Stop();
             Console.WriteLine();
