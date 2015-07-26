3461ee5c6f1ff91dd7682fc1e436c98e72f568d5
lukan@xknife.net <lukan@xknife.net>
2015-07-26 23:19:35 +0800

    完善细节

--- NKnife.Scpi/ScpiCommand.cs
+++ NKnife.Scpi/ScpiCommand.cs
@@ -1,4 +1,6 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
+using System.Text;
 using System.Xml;
 
 namespace ScpiKnife
@@ -36,21 +38,58 @@ namespace ScpiKnife
             return command;
         }
 
+        public static void Build(ScpiCommand command, ref XmlElement element)
+        {
+        }
+
         public ScpiCommand()
         {
             Interval = 200;
             IsHex = false;
         }
 
+        /// <summary>
+        /// 命令主体
+        /// </summary>
         public string Command { get; set; }
 
+        /// <summary>
+        /// 命令的等待周期,指等待仪器返回结果的超时时间
+        /// </summary>
         public long Interval { get; set; }
 
+        /// <summary>
+        /// 命令需要仪器返回数据
+        /// </summary>
         public bool IsReturn { get; set; }
 
+        /// <summary>
+        /// 命令主体是用原生字符串表达,还是16进制字符串表达
+        /// </summary>
         public bool IsHex { get; set; }
 
+        /// <summary>
+        /// 命令的解释
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// 生成协议字节数组
+        /// </summary>
+        /// <param name="gpibAddress">协议体里的GPIB地址</param>
+        /// <returns>协议字节数组</returns>
+        public virtual byte[] GenerateProtocol(int gpibAddress)
+        {
+            byte mainCommand = IsReturn ? (byte) 0xAA : (byte) 0xAB;
+            const byte SUB_COMMAND = 0x00;
+            var scpiBytes = Encoding.ASCII.GetBytes(Command);
+
+            var bs = new byte[] {0x08, (byte) gpibAddress, (byte) (scpiBytes.Length + 2), mainCommand, SUB_COMMAND};
+            var result = new byte[bs.Length + scpiBytes.Length];
+            Buffer.BlockCopy(bs, 0, result, 0, bs.Length);
+            Buffer.BlockCopy(scpiBytes, 0, result, bs.Length, scpiBytes.Length);
+            return result;
+        }
 
         public override string ToString()
         {
