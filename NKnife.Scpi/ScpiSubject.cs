d207ef78b38575459a2b67cb832790532cab4ca3
lukan@xknife.net <lukan@xknife.net>
2015-10-10 17:24:20 +0800

    更改SCPI相关类型，以保证能够修改后保存

--- NKnife.Scpi/ScpiSubject.cs
+++ NKnife.Scpi/ScpiSubject.cs
@@ -1,12 +1,14 @@
-﻿namespace ScpiKnife
+﻿using System.Xml;
+
+namespace ScpiKnife
 {
     /// <summary>
-    /// 面向一个工作主题的指令集合
+    /// 面向一个工作指令主题的指令集合
     /// </summary>
     public class ScpiSubject
     {
         /// <summary>
-        /// 工作主题的描述
+        /// 工作指令主题的描述
         /// </summary>
         public string Description { get; set; }
         /// <summary>
@@ -17,5 +19,15 @@
         /// 采集指令集合
         /// </summary>
         public ScpiGroup Collect { get; set; }
+
+        /// <summary>
+        /// 指令集合所在的主题集合
+        /// </summary>
+        public ScpiSubjectCollection OwnerCollection { get; set; }
+
+        /// <summary>
+        /// 指令集合所关联的XML节点
+        /// </summary>
+        public XmlElement XmlElement { get; set; }
     }
 }
--- NKnife.Scpi/ScpiSubjectCollection.cs
+++ NKnife.Scpi/ScpiSubjectCollection.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 
 namespace ScpiKnife
 {
     /// <summary>
-    /// 工作主题集合
+    /// 指令主题集合
     /// </summary>
     public class ScpiSubjectCollection : List<ScpiSubject>
     {
+        /// <summary>
+        /// 指令主题所属的仪器品牌
+        /// </summary>
+        public string Brand { get; set; }
+
+        /// <summary>
+        /// 指令主题所属的仪器型号
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 指令集合所在的文件
+        /// </summary>
+        public FileInfo File { get; set; }
     }
 }
