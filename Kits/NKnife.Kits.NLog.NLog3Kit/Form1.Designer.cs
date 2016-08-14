3668c468d5e153c204e8f05888647d6caa6fc6a1
lukan <lukan@xknife.net>
2016-08-14 20:50:25 +0800

    新建nlog封装测试项目

---
+++ Kits/NKnife.Kits.NLog.NLog3Kit/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace NKnife.Kits.NLog.NLog3Kit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // logPanel1
+            // 
+            this.logPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logPanel1.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.logPanel1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Clickable;
+            this.logPanel1.Location = new System.Drawing.Point(0, 0);
+            this.logPanel1.Name = "logPanel1";
+            this.logPanel1.Size = new System.Drawing.Size(635, 376);
+            this.logPanel1.TabIndex = 0;
+            this.logPanel1.ToolStripVisible = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(635, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(100, 22);
+            this.toolStripButton1.Text = "日志显示测试";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 376);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.logPanel1);
+            this.Name = "Form1";
+            this.Text = "NLog3Kit";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NLog3.Controls.LogPanel logPanel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ Kits/NKnife.Kits.NLog.NLog3Kit/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NKnife.GUI.WinForm;
+
+namespace NKnife.Kits.NLog.NLog3Kit
+{
+    public partial class Form1 : SimpleForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
