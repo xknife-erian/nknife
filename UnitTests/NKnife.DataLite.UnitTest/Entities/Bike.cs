2dbb0af3cd543854a0dc320a16717cb74db2ce57
lukan@xknife.net <7uu7uicbc33442>
2017-05-09 21:30:15 +0800

    ...

---
+++ UnitTests/NKnife.DataLite.UnitTest/Entities/Bike.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NKnife.DataLite.UnitTest.Entities
+{
+    public class Bike
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public DateTime ProductionDate { get; set; }
+    }
+}
