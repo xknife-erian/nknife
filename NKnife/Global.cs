0b517d65da6832616602ea80219182bd857880e4
lukan@xknife.net <7uu7uicbc33442>
2017-05-11 21:24:02 +0800

    Nuget version: 17.2.0.511

--- NKnife/Global.cs
+++ NKnife/Global.cs
@@ -2,12 +2,6 @@
 {
     public static class Global
     {
-        private static string _Culture = "zh-CN";
-
-        public static string Culture
-        {
-            get { return _Culture; }
-            set { _Culture = value; }
-        }
+        public static string Culture { get; set; } = "zh-CN";
     }
 }
