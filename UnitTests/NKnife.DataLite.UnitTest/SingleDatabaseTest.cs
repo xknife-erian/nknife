8830c64da8bbc79670c3d112d4f076a50a2522cf
lukan@xknife.net <7uu7uicbc33442>
2017-05-16 22:39:25 +0800

    单数据多集合单元测试完成。其中发现当Id是int型时，不能设置为0.

--- UnitTests/NKnife.DataLite.UnitTest/SingleDatabaseTest.cs
+++ UnitTests/NKnife.DataLite.UnitTest/SingleDatabaseTest.cs
@@ -31,19 +31,19 @@ namespace NKnife.DataLite.UnitTest
             bikes.Count.Should().Be(0);
             meters.Count.Should().Be(0);
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 1; i <= 100; i++)
             {
-                var bike = new Bike();
-                bike.Id = i;
-                bike.ProductionDate = DateTime.Now;
-                bikes.Save(bike).Should().BeTrue();
-
                 var meter = new Meter();
                 meter.Id = $"{i}";
                 meters.Save(meter).Should().BeTrue();
 
-                bikes.Count.Should().Be(i + 1);
-                meters.Count.Should().Be(i + 1);
+                var bike = new Bike();
+                bike.Id = i*i;
+                bike.ProductionDate = DateTime.Now;
+                bikes.Save(bike).Should().BeTrue();
+
+                bikes.Count.Should().Be(i);
+                meters.Count.Should().Be(i);
             }
             bikes.Count.Should().Be(100);
             meters.Count.Should().Be(100);
