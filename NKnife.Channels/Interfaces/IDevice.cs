612a9af67978b1b82ebaa70ee8eda514fd008312
lukan@xknife.net <7uu7uicbc33442>
2017-08-30 23:07:29 +0800

    nuget: 17.5.1.830-17.5.4.830

--- NKnife.Channels/Interfaces/IDevice.cs
+++ NKnife.Channels/Interfaces/IDevice.cs
@@ -6,9 +6,14 @@
     public interface IDevice
     {
         /// <summary>
-        ///     品牌
+        ///     生产厂商
         /// </summary>
-        string Brand { get; set; }
+        string Manufacturer { get; set; }
+
+        /// <summary>
+        ///     型号
+        /// </summary>
+        string Model { get; set; }
 
         /// <summary>
         ///     设备名称
@@ -24,5 +29,10 @@
         ///     设备地址
         /// </summary>
         int Address { get; set; }
+
+        /// <summary>
+        ///     设备说明或信息
+        /// </summary>
+        string Information { get; set; }
     }
 }
--- NKnife/Properties/AssemblyVersion.cs
+++ NKnife/Properties/AssemblyVersion.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 
 // 有关程序集的常规信息通过下列属性集控制。
 
-[assembly: AssemblyVersion("17.5.1.830")]
-[assembly: AssemblyFileVersion("17.5.1.830")]
+[assembly: AssemblyVersion("17.5.4.830")]
+[assembly: AssemblyFileVersion("17.5.4.830")]
