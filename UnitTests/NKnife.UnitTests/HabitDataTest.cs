f95ee58dfae53f570f3b03ec29bcd4f9f8b3a5ff
lukan-home <lukan@xknife.net>
2022-03-10 22:41:05 +0800

    update.

---
+++ UnitTests/NKnife.UnitTests/HabitDataTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace NKnife.UnitTests
+{
+    public class HabitDataTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            var hd = new HabitData();
+            hd.SetValue("abcd", "abcd");
+            hd.GetValue("abcd","1234").Should().Be("abcd");
+        }
+    }
+
+}
