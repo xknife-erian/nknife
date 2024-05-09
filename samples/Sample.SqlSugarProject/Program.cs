ac037f47a2e0b21521ee24acda61fb373b039200
lukan <lukan@live.cn>
2024-05-09 21:18:28 +0800

    准备学习一下SqlSugar，以确保知识构架的完整性

---
+++ samples/Sample.SqlSugarProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleUi;
+
+namespace Sample.SqlSugarProject
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, SqlSugar! 2024-5-9");
+
+            var menuRun = new ConsoleUi.Console.ConsoleMenuRunner();
+            var menu    = new SqlSugarLearningMenu();
+            await menuRun.Run(menu);
+        }
+    }
+
+    public class SqlSugarLearningMenu : SimpleMenu
+    {
+        
+    }
+}
