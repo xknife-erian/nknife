4ad4e8b0e3b74def3a83e69a918fbe416d8c2dbe
lukan <lukan@live.cn>
2024-05-10 14:56:53 +0800

    in progress.

---
+++ samples/Sample.SqlSugarProject/SqlSugarLearningMenu.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleUi;
+
+namespace Sample.SqlSugarProject
+{
+    public partial class SqlSugarLearningMenu : SimpleMenu
+    {
+        public void Basic(IMenuContext ctx) { }
+
+        public void SqlSugarQuery(IMenuContext ctx) { }
+        public void SqlSugarJoin(IMenuContext ctx) { }
+        public void SqlSugarPage(IMenuContext ctx) { }
+        public void SqlSugarFilter(IMenuContext ctx) { }
+        public void SqlSugarTransaction(IMenuContext ctx) { }
+        public void SqlSugarProcedure(IMenuContext ctx) { }
+        public void SqlSugarAttribute(IMenuContext ctx) { }
+
+    }
+}
